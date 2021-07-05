d337413482b27a1bbf9a64fc49a09f66bd80bac2
VelinovAngel <kakarum1992@gmail.com>
2021-07-05 23:47:09 +0200

    Add class library data

--- BikesBooking/BikerBooking.Data/BikerDbContext.cs
+++ BikesBooking/BikerBooking.Data/BikerDbContext.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BikerBooking.Data
+﻿namespace BikerBooking.Data
 {
-    class BikerDbContext
+    public class BikerDbContext
     {
     }
 }
