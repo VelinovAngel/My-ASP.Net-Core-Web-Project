ed2e08ad3ba314069ced308b8f5de0990773abc8
VelinovAngel <kakarum1992@gmail.com>
2021-07-16 23:55:01 +0200

    Implemented MotorcycleService and apposit methods

---
+++ Services/BikesBooking.Services.Data/DTO/CreateMotorcycleDto.cs
@@ -0,0 +1,23 @@
+﻿namespace BikesBooking.Services.Data.DTO
+{
+    public class CreateMotorcycleDto
+    {
+        public string Manufacturer { get; set; }
+
+        public int Year { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int CubicCentimetre { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Available { get; set; }
+
+        public string Url { get; set; }
+
+        public string Review { get; set; }
+    }
+}
