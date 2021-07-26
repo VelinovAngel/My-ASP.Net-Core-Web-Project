3ab2375a130baf36e1188e3d9e092729a7ce2c41
VelinovAngel <kakarum1992@gmail.com>
2021-07-26 23:45:28 +0200

    Add new migration where set nullable bookingId in entity Offer

---
+++ Data/BikesBooking.Data/Migrations/20210726214432_ChangeEntityForeingKeyBookingIdNullable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikesBooking.Data.Migrations
+{
+    public partial class ChangeEntityForeingKeyBookingIdNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Offers_BookingId",
+                table: "Offers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookingId",
+                table: "Offers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_BookingId",
+                table: "Offers",
+                column: "BookingId",
+                unique: true,
+                filter: "[BookingId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Offers_BookingId",
+                table: "Offers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookingId",
+                table: "Offers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_BookingId",
+                table: "Offers",
+                column: "BookingId",
+                unique: true);
+        }
+    }
+}
