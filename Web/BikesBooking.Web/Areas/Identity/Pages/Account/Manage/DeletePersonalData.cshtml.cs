df0f7bff774d506aa450bff5ced0857416ced65f
VelinovAngel <kakarum1992@gmail.com>
2021-08-14 19:14:05 +0200

    Fix problem with deleting users

---
+++ Web/BikesBooking.Web/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -0,0 +1,102 @@
+﻿namespace BikesBooking.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using BikesBooking.Data.Common.Repositories;
+    using BikesBooking.Data.Models;
+    using BikesBooking.Web.Infrastructure;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    public class DeletePersonalDataModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<DeletePersonalDataModel> logger;
+        private readonly IDeletableEntityRepository<Client> clientManager;
+
+        public DeletePersonalDataModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<DeletePersonalDataModel> logger,
+            IDeletableEntityRepository<Client> clientManager)
+        {
+            if (clientManager is null)
+            {
+                throw new ArgumentNullException(nameof(clientManager));
+            }
+
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.clientManager = clientManager;
+        }
+
+        public bool RequirePassword { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            this.RequirePassword = await this.userManager.HasPasswordAsync(user);
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            this.RequirePassword = await this.userManager.HasPasswordAsync(user);
+            if (this.RequirePassword)
+            {
+                if (!await this.userManager.CheckPasswordAsync(user, this.Input.Password))
+                {
+                    this.ModelState.AddModelError(string.Empty, "Incorrect password.");
+                    return this.Page();
+                }
+            }
+
+            var userRole = await this.userManager.GetRolesAsync(user);
+
+            var role = await this.userManager.RemoveFromRolesAsync(user, userRole);
+            var currUser = this.clientManager.All().FirstOrDefault(x => x.UserId == this.User.GetId());
+            this.clientManager.Delete(currUser);
+            var result = await this.userManager.DeleteAsync(user);
+            var userId = await this.userManager.GetUserIdAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred deleting user with ID '{userId}'.");
+            }
+
+            await this.signInManager.SignOutAsync();
+
+            this.logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
+
+            return this.Redirect("~/");
+        }
+
+
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
